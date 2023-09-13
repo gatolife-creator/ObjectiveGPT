9335eb2c33d0352987c19a5ed0506f95176ae2da
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-09-13 19:57:57 +0900

    Prompt and task data format modified

--- src/content/index.ts
+++ src/content/index.ts
@@ -6,46 +6,89 @@ startButton.style.right = '120px'
 startButton.className =
   'btn btn-neutral whitespace-nowrap text-gray-700 shadow-[0px_1px_6px_0px_rgba(0,0,0,0.02)] dark:text-gray-300 md:whitespace-normal'
 document.body.appendChild(startButton)
-startButton.onclick = () => start(5)
-
-const stopButton = document.createElement('button')
-stopButton.textContent = 'STOP'
-stopButton.style.position = 'fixed'
-stopButton.style.top = '25px'
-stopButton.style.right = '50px'
-stopButton.className =
-  'btn btn-neutral whitespace-nowrap text-gray-700 shadow-[0px_1px_6px_0px_rgba(0,0,0,0.02)] dark:text-gray-300 md:whitespace-normal'
-document.body.appendChild(stopButton)
-stopButton.onclick = () => stop()
+startButton.onclick = () => start(1)
 
 // ---------------------------------------------------------------------------//
 
-let isStopped = false
 let report = ''
 let lang = 'ja'
 
-const listUpTasks = (command: string) => {
-  let tasks: (string | null)[] = []
-  let baseCommand = ''
-  baseCommand = `
+type TaskTree = {
+  name: string
+  subtask: TaskTree[]
+}
+
+const listUpTasks = (goal: string) => {
+  let tasks: TaskTree[] = []
+  let prompt = ''
+  prompt = `
 Please list up the tasks you need to do to achieve the goal below.
 
 Rules:
-・Answer in ${lang}
-・Concrete answer is not needed. Just answer tasks.
-・Please be sure to use this format
+* Answer in ${lang} except for titles
+* Concrete answer is not needed. Just answer tasks.
+* Please be sure to use this format
     ## Tasks
     <Task list would be here using markdown like "1. and 2. ">
 
 Goal:
+${goal}
 `
-  send((baseCommand + command) as string)
+  send(prompt)
 
-  return new Promise((resolve) => {
+  return new Promise<TaskTree[]>((resolve) => {
     const interval = setInterval(() => {
       if (!document.querySelector('.result-streaming')) {
         tasks = Array.from(getLatestConversation().querySelectorAll('li')).map(
-          (elem) => elem.textContent,
+          (elem) =>
+            ({
+              name: elem.textContent,
+              subtask: [],
+            } as TaskTree),
+        )
+        clearInterval(interval)
+        resolve(tasks)
+      }
+    }, 3000)
+  })
+}
+
+const splitTask = async (goal: string, task: TaskTree) => {
+  let tasks: TaskTree[] = []
+  let prompt = ''
+  prompt = `
+Please split this task into small pieces if you can.
+If you cannot split into small pieces, just answer "false."
+You "must" answer concrete tasks like coding or documenting. No abstract tasks like considering, thinking about, etc.
+
+Rules:
+* Answer in ${lang} except for titles
+* Please take an account the goal "${goal}"
+* Concrete answer is not needed. Just answer tasks
+* Please use STRONG imperative sentences
+* Please pretend to be a boss who is really strict and order me the tasks
+* Please be sure to use this format
+    If you can split it into small pieces:
+        ## Smaller Tasks
+        <Concrete task list would be here using markdown like "1. and 2. ">
+    Else if you cannot split it into small pieces:
+        false
+
+Task:
+${task.name}
+`
+
+  send(prompt)
+  return new Promise<TaskTree[]>((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (!document.querySelector('.result-streaming')) {
+        if (getLatestConversation().textContent?.includes('false')) {
+          clearInterval(interval)
+          reject("You can't split this task into small pieces")
+        }
+        console.log(getLatestConversation())
+        tasks = Array.from(getLatestConversation().querySelectorAll('li')).map(
+          (elem) => ({ name: elem.textContent, subtask: [] } as TaskTree),
         )
         clearInterval(interval)
         resolve(tasks)
@@ -56,38 +99,29 @@ Goal:
 
 const execTask = async (goal: string, task: string) => {
   let solution = ''
-  let baseCommand = ''
-  baseCommand = `Please do following task, and you need to follow the rules below:
+  let prompt = ''
+  prompt = `Please do following task, and you need to follow the rules below:
     
     Rules:
-    ・Answer in ${lang}
-    ・Please take an account the goal "${goal}"
-    ・Please do coding as long as you can
-    ・Do not provide abstract answer, but answer with concrete solution and even do the work as long as you can
-    ・Please rate your task progress you've done, using following template
-        - Does your work get useful literally "right away?" [0 to 10]
-        - Is your answer literally "concrete?" [0 to 10]
-    ・You "must" use this format:
+    * You "must" answer in ${lang} except for titles which is marked by "#", "##" and something like that
+    * Please take an account the goal "${goal}"
+    * Please do coding as long as you can
+    * Do not provide abstract answer, but answer with concrete solution and even do the work as long as you can
+    * You "must" use this format and other formats are not allowed:
         ## <Task name would be here>
         ### Solutions
         <Concrete solutions would be here>
         if sample code is needed:
             ### Sample Code
             <Sample code would be here (Provide more concrete code as long as you can)>
-        ### Execution Results
-        <Execution results would be here>
         ### Possible Risks
         <Possible risks would be here>
-        ### Possible Improvements
-        <Possible improvements would be here>
-        ### Task Progress
-        <Display progress bar here. Use "⭐️" to represent 1 point, use "☆" to represent 0 point. Maximum stars count would be 10.>
-        <State why you rated it that way>
     Task:
+    ${task}
     `
-  send(baseCommand + task)
+  send(prompt)
 
-  return new Promise((resolve) => {
+  return new Promise<string>((resolve) => {
     const regenerateButton = document.querySelector('form')!.querySelector('button')
     const interval = setInterval(() => {
       if (
@@ -115,25 +149,28 @@ const main = async () => {
   const firstCommand = document.querySelector('textarea')?.textContent as string
   report += firstCommand
 
-  const tasks = (await listUpTasks(firstCommand)) as string[]
+  const tasks = await listUpTasks(firstCommand)
   console.log(tasks)
   for (const task of tasks) {
-    const solution = await execTask(firstCommand, task)
-    console.log(task, solution)
-    report += '\n' + task + '\n' + solution
+    try {
+      task.subtask.push(...(await splitTask(firstCommand, task)))
+      console.log(task)
+    } catch (e) {
+      console.log(e)
+      continue
+    }
+    // const solution = await execTask(firstCommand, task)
+    // console.log(task, solution)
+    // report += '\n' + task + '\n' + solution
   }
-  console.log(report)
+  console.log(tasks)
+  //   console.log(report)
 }
 
 const start = async (n: number) => {
   for (let i = 0; i < n; i++) {
-    if (isStopped) break
     await main()
   }
-}
-
-const stop = () => {
-  isStopped = true
 }
 
 const send = (message: string) => {
